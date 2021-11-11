2ba163c7a27d8ad3f088f1b35c5349bff2f03ee2
Alexander Strasser <a.strasser@tutanota.de>
2021-11-11 22:37:43 +0100

    colored street differentiation in mpl visualisation

--- src/main.py
+++ src/main.py
@@ -8,27 +8,48 @@ if __name__ == '__main__':
     import networkx as nx
     import pandas as pd
 
+    #from palettable.scientific.sequential import Dark2_7
 
     # get some data
-    place = "49.49671,8.47955"
+    place = "49.3165, 8.4527"
     distance=4000
     buildings = ox.geometries_from_address(place,{"building":True}, dist=distance)
+    landuse = ox.geometries_from_address(place,{"landuse":True}, dist=distance)
  
-    streets = ox.graph_from_address(place,dist=distance, network_type="drive")
-    rails = ox.graph_from_address(place,dist=distance, custom_filter='["railway"~"rail"]')
-    graph =nx.compose(streets,rails)
-
+    #streets = ox.graph_from_address(place,dist=distance, network_type="drive")
+    streets = ox.graph_from_address(place,dist=distance)
+    #rails = ox.graph_from_address(place,dist=distance, custom_filter='["railway"~"rail"]')
+    #graph =nx.compose(streets,rails)
+    graph=streets
     
+    
+    #draw graph
+    colors= [
+    "r" if 'highway' in d and d['highway']=='motorway' 
+    else "orange" if 'highway' in d and d['highway']=='trunk'
+    else "yellow" if 'highway' in d and d['highway']=='primary'
+    else "yellow" if 'highway' in d and d['highway']=='secondary'
+    else "w" if 'highway' in d and d['highway']=='tertiary'
+    else "w" if 'highway' in d and d['highway']=='unclassified'
+    else "w" if 'highway' in d and d['highway']=='residential'
+    else 'grey' if 'highway' in d 
+    else 'blue' for _, _, _, d in graph.edges(keys=True, data=True)
+    ]
 
-    colors= ['w' if 'highway' in d else 'r' for _, _, _, d in graph.edges(keys=True, data=True)]
-    fig, ax = ox.plot_graph(graph,show=False,edge_color=colors)
+    #colors=ox.plot.get_colors(cmap="Greys")
+    fig, ax = ox.plot_graph(graph,show=False,edge_color=colors,node_size=0)
 
-    #landuse.plot(column='landuse', ax=ax)
+    #draw streets
+    #fig, ax = ox.plot_figure_ground(point=(49.3165, 8.4527),dist=distance, dpi=40, save=False, show=True, close=False)
+
+    #draw land and buildings
+    #landuse.plot(column='landuse', ax=ax,cmap= "Greens_r")
+    buildings.plot(ax=ax,color="cyan")
     #buildings.plot(ax=ax, color='None',lw=1, alpha=1)
-
-
-    buildings.plot(ax=ax)
-
+    
+    nodes, edges = ox.graph_to_gdfs(streets)
+    print(edges)
+    
     #ax.axis('off')
     plt.show()
     #fig.show()
